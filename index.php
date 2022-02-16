22e52b598521b8e14b7e6757d57134e0dd8dc61e
Baptiste LEJEUNE <baptiste.lejeune@hotmail.com>
2022-02-16 16:26:08 +0100

    test bdd

--- index.php
+++ index.php
@@ -1,3 +1,21 @@
 <?php
 
-echo 'Hello world !';
+// echo 'Hello world !';
+
+
+$bdd = new PDO("mysql:dbname=coffee;
+host=mysql-69240-0.cloudclusters.net:15630;
+charset=utf8",
+"admin",
+"46r2WkZw");
+
+$res = $bdd->query("SELECT * FROM waiter");
+
+
+// var_dump($res->fetchAll());
+
+$waiter = $res->fetchAll();
+
+foreach($waiter as $name){
+    echo $name['name'] . "<br>";
+}
