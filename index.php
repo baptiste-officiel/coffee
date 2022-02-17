73e573c6bc1fd024bb6a11593e6624d1af4bc99e
Baptiste LEJEUNE <baptiste.lejeune@hotmail.com>
2022-02-17 09:34:32 +0100

    test bdd

--- index.php
+++ index.php
@@ -1,13 +1,22 @@
 <?php
 
-// echo 'Hello world !';
+// host : mysql-69240-0.cloudclusters.net
+// port : 15630
+// username : admin 
+// password : 46r2WkZw
 
 
-$bdd = new PDO("mysql:dbname=coffee;
-host=mysql-69240-0.cloudclusters.net:15630;
+$bdd = new PDO("mysql:host=mysql-69240-0.cloudclusters.net:15630;
 charset=utf8",
 "admin",
 "46r2WkZw");
+
+
+// var_dump($bdd);
+
+
+
+
 
 $res = $bdd->query("SELECT * FROM waiter");
 
